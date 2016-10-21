abd0176be3b6db5a4d3f170f82f965f3ace26f64
Martin Kjems <mk@issuu.com>
2016-10-21 14:10:24 +0200

    Added linked in link

--- client/src/frontpage/components/Contact.js
+++ client/src/frontpage/components/Contact.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 const Iconbusinessman = require('../assets/businessman_in_circle.svg');
 
+const ContactInfo = () => {
+	return (
+		<p>
+			Phone: <a href="tel:(+45) 51 24 52 65">(+45) 51 24 52 65</a><br />
+			LinkedIn : <a href="https://www.linkedin.com/in/martin-kjems-39a9a6">martin-kjems-39a9a6</a><br />
+		</p>
+
+	);
+}
+
 const Contact = ({onContactClick, showContact}) => {
 	return (
 		<section className="sec_01">
@@ -11,7 +21,7 @@ const Contact = ({onContactClick, showContact}) => {
 		    <div className="section-text">
 		        <h2>Web developer</h2>
 		        {(showContact)
-		        	? <p>Phone: <a href="tel:(+45) 51 24 52 65">(+45) 51 24 52 65</a><br /><br /></p>
+		        	? <ContactInfo />
 		        	: <p >Im a web developer with many years of experience</p>
 		        }
 		        <button className="positive" onClick={onContactClick}>Contact information</button>
